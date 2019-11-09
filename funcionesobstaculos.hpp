c9564119f92e89743d33a2fb935ac08627d25448
Kaled-Hernandez <57291170+Kaled-Hernandez@users.noreply.github.com>
2019-11-09 12:08:03 -0300

    Add files via upload

---
+++ funcionesobstaculos.hpp
@@ -0,0 +1,16 @@
+#ifndef __FUNCIONOBS__
+#define __FUNCIONOBS__
+
+
+#include "funciones.hpp"
+#include <Arduino.h>
+
+void detenerBoton();
+void giroIz();
+void giroDer();
+void obstaculoDer();
+void obstaculoIz();
+void ajustarAdelante();
+void ajustarAtras();
+
+#endif
